f768380e4c7ad0f972b658d24149d48545369cb0
imdylanpro <159092190+imdylanpro@users.noreply.github.com>
2024-02-06 18:48:18 -0800

    Update slices.py

--- slices.py
+++ slices.py
@@ -2,3 +2,11 @@
 # Dylan Nelson
 # February 6, 2024
 
+# Print the message "The first three items in the list are:", then use a slice to print the first three items from the list.
+
+cars = ["honda", "ford", "mazda", "toyota", "subaru"]
+
+
+print(f"The first three items in the list are:", cars[0:3])
+
+# Print the message "The middle items in the list are:", then use a slice to print the 3 middle items in the list
